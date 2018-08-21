a2290e912dba9cec6ae992d226d5f92e865299f7
Momin M. Malik <momin.malik@cs.cmu.edu>
2018-08-21 13:08:13 -0400

    Update ccdf.R

--- ccdf.R
+++ ccdf.R
@@ -14,20 +14,29 @@
 #   volume = {51},
 #   year = {2009}
 # }
+# 
+# There's a lot to fix to make this a flexible function, but I've re-used this
+# code a lot for myself and want to share it. 
+# 
 # Plotting parameters are designed to look somewhat like what MATLAB produces,
 # but (hopefully) nicer.
+# 
 # CAUTION: This code does not have much built-in flexibility yet. I made it for
 # for long-tailed count data, so for example you would need to change some
 # parameters if you have x-values between 0 and 1. 
+# 
 # TODO: Make ifelse for table input vs vector input
 # TODO: Make ifelse for only one or neither axis in log scale
 # TODO: Add min for x-axis grid and labels, for x values between 0 and 1
-# TODO: Cut off additional x-axis ticks above max
+# TODO: More carefully cut off additional x-axis ticks above max
+# TODO: Make option for lines instead of lines(type = "b"), and for color
+# TODO: Automatic axis las and cex determination, maybe push xlab and ylab out
+# TODO? Instead of cuting off last x-axis point, shift x-values back
+# and plot P(X >= x)? Or make option to do that?
+# 
 # (c) Momin M. Malik 2018
 # v0.9, 24 July 2018
 ############################################################################
-
-
 
 # Pretty printing of scientific notation, via
 # https://stackoverflow.com/questions/29785555/in-r-using-scientific-notation-10-rather-than-e
@@ -44,17 +53,15 @@ changeSciNot <- function(n) {
 # ccdf <- function(count, freq, ...) {
 #   x <- count
 #   y <- 1 - cumsum(freq)/sum(freq)
+# Otherwise, the function inputs the vector of the distribution
 
 # # If either the count of frequency is better plotted in linear scale,
-# # you can replace code internally, e.g., log="y", and:
+# # you will need to replace code internally, e.g., log="y", and:
 # axis(1,
 #      seq(5,50,5),
 #      labels = as.character(seq(5,50,5)))
 
 # You might need to adjust the cex and/or las of the x-axis, axis(1,...)
-
-# TODO? Instead of cuting off last x-axis point, shift x-values back
-# and plot P(X >= x)? 
 
 ccdf <- function(x, ...) {
   tmp <- table(x)
