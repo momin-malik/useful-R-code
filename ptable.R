1151b5d9aebb3052759fc23db32816931e5c4e3c
Momin M. Malik <momin.malik@gmail.com>
2018-11-13 13:46:30 -0500

    Update ptable.R

--- ptable.R
+++ ptable.R
@@ -11,13 +11,14 @@ ptable <- function(x, y, ...) {
   tmp1 <- addmargins(table(x, y, ...), margin=1:2)
   dimnames(tmp1)[[1]][length(dimnames(tmp1)[[1]])] <- "Total"
   dimnames(tmp1)[[2]][length(dimnames(tmp1)[[2]])] <- "Total"
-  tmp2 <- round(100*addmargins(prop.table(tmp1, margin = 2), margin = 1), digits = 1)
-  dimnames(tmp2)[[1]][length(dimnames(tmp2)[[1]])] <- "Total"
+  tmp2 <- round(100*addmargins(prop.table(table(x, y, ...), margin = 2), margin = 1), digits = 1)
   tmp1 <- as.matrix(tmp1)
   tmp2 <- as.matrix(tmp2)
   tmp2 <- cbind(tmp2, Total = round(100*tmp1[,dim(tmp1)[2]]/tmp1[length(tmp1)],1))
-  tmp <- `dim<-`(sprintf('%g (%g%s)', as.matrix(tmp1), as.matrix(tmp2), "%"), dim(tmp1))
+  tmp <- `dim<-`(sprintf('%s (%s%s)', tmp1, tmp2, "%"), dim(tmp1))
   rownames(tmp) <- dimnames(tmp1)[[1]]
   colnames(tmp) <- dimnames(tmp1)[[2]]
+  # tmp <- rbind(tmp[-nrow(tmp),], `---` = "---", Total = tmp[nrow(tmp),])
+  # tmp <- cbind(tmp[,-ncol(tmp)], `|` = "|", Total = tmp[,ncol(tmp)])
   return(noquote(tmp))
 }
